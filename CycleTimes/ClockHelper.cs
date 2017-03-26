1554c15d254ce3b491635e0dcfde973479298987
mikhaild908 <mikhaild908@gmail.com>
2017-03-26 06:30:08 -0700

    first commit
    first commit

---
+++ CycleTimes/ClockHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+
+namespace CycleTimes
+{
+    public static class ClockHelper
+    {
+        private const int DEGREES_PER_HOUR = 30;
+        private const int DEGREES_PER_MINUTE = 6;
+        private const int DEGREES_PER_SECOND = 6;
+        private const double DEGREES_OF_HOUR_HAND_PER_MINUTE = 0.5;
+
+        public static Point GetEndPoint(ClockHand clockHand, int handLength, DateTime dateTime, Point origin)
+        {
+            var hour = dateTime.Hour;
+            var minute = dateTime.Minute;
+            var second = dateTime.Second;
+
+            switch (clockHand)
+            {
+                case ClockHand.Hour:
+                    return AddXYOffset(GetEndPoint(handLength, 90 - (hour * DEGREES_PER_HOUR + minute * DEGREES_OF_HOUR_HAND_PER_MINUTE)), origin);
+                case ClockHand.Minute:
+                    return AddXYOffset(GetEndPoint(handLength, 90 - minute * DEGREES_PER_MINUTE), origin);
+                case ClockHand.Second:
+                    return AddXYOffset(GetEndPoint(handLength, 90 - second * DEGREES_PER_SECOND), origin);
+            }
+
+            throw new Exception("Invalid clock hand selection");
+        }
+
+        public static Point GetEndPoint(int handLength, double degrees)
+        {
+            return new Point(handLength * Math.Cos(Math.PI / 180 * degrees), -1 * handLength * Math.Sin(Math.PI / 180 * degrees));
+        }
+
+        private static Point AddXYOffset(Point point, Point origin)
+        {
+            point.X = origin.X + point.X;
+            point.Y = origin.Y + point.Y;
+
+            return point;
+        }
+    }
+}
---
+++ CycleTimes/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace CycleTimes
+{
+    public partial class MainWindow : Window
+    {
+        private const string CENTRAL_STANDARD_TIME_ID = "Central Standard Time";
+        private const int HOUR_HAND_LENGTH = 50;
+        private const int MINUTE_HAND_LENGTH = 75;
+        private const int SECOND_HAND_LENGTH = 75;
+
+        private readonly Point ORIGIN = new Point(0, 100);
+        private readonly TimeZoneInfo _centralZone = TimeZoneInfo.FindSystemTimeZoneById(CENTRAL_STANDARD_TIME_ID);
+
+        DispatcherTimer _timer = new DispatcherTimer();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeTimer();
+        }
+
+        private void InitializeTimer()
+        {
+            _timer.Tick += _timer_Tick; ;
+            _timer.Interval = new TimeSpan(0, 0, 1);
+            _timer.Start();
+        }
+
+        private void _timer_Tick(object sender, EventArgs e)
+        {
+            var centralTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, _centralZone);
+
+            var hourXY = ClockHelper.GetEndPoint(ClockHand.Hour, HOUR_HAND_LENGTH, centralTime, ORIGIN);
+            hourHand.Data = new LineGeometry(ORIGIN, hourXY);
+
+            var minuteXY = ClockHelper.GetEndPoint(ClockHand.Minute, MINUTE_HAND_LENGTH, centralTime, ORIGIN);
+            minuteHand.Data = new LineGeometry(ORIGIN, minuteXY);
+
+            var secondXY = ClockHelper.GetEndPoint(ClockHand.Second, SECOND_HAND_LENGTH, centralTime, ORIGIN);
+            secondHand.Data = new LineGeometry(ORIGIN, secondXY);
+
+            cst.Text = centralTime.ToLongTimeString() + " CST";
+        }
+    }
+}
